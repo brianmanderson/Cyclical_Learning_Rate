5829d6661e62507d08486a63b53b957e46dc92d1
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-30 10:47:20 -0600

    default back to 1

--- clr_callback_TF2.py
+++ clr_callback_TF2.py
@@ -615,7 +615,7 @@ class SGDRScheduler(Callback):
         '''Check for end of current cycle, apply restarts when necessary.'''
         if epoch + 1 == self.next_restart:
             self.batch_since_restart = 0
-            if self.mult == 1:
+            if self.mult != -1:
                 self.cycle_length = np.ceil(self.cycle_length * self.mult_factor)
                 self.next_restart += self.cycle_length
                 self.max_lr *= self.lr_decay
